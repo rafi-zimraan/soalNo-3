5e3cb34ca1628f9aea5b4e82383aaf308b4bb476
rafi-zimraan <raffizimraan27@gmail.com>
2023-02-16 17:29:16 +0700

    first commit

---
+++ soal 2.js
@@ -0,0 +1,11 @@
+// SOAL NO. 3
+let panjang =(10);
+let lebar = (20);
+
+let hasil = (panjang + lebar);
+
+let gabungan =(hasil + "perhitungan");
+function lebarPersegiPanjang(panjang , lebar) {
+    return panjang * lebar;
+}
+console.log(lebarPersegiPanjang(panjang,lebar));
